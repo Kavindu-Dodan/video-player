6ba66664d9e7ce3b9123ba07360064f2e8c8eb31
Kavindu-dodan <kavindudodanduwa@gmail.com>
2018-09-15 21:28:56 +0530

    Managed to run a video segment sent from server
    ToDo - check how to store and play on demand

--- Server/src/app.py
+++ Server/src/app.py
@@ -9,21 +9,37 @@ from yaml import load
 class VideoServer(object):
 
     def __init__(self):
-        self.video_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'videos/video-list.yml'))
+        self.video_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'videos')) + '/'
 
-        with open(self.video_path, 'r') \
+        # We only have one video yet
+        with open(self.video_path + 'video-list.yml', 'r') \
                 as fileStream:
             self.video_list = load(fileStream)['videos']
 
         cherrypy.log("Video list loaded")
 
     def _cp_dispatch(self, vpath):
+        cherrypy.request.params["path"] = []
+
+        while len(vpath) > 0:
+            cherrypy.request.params["path"].append(vpath.pop(0))
+
         return self
 
-    def GET(self, **params):
-        res = dict()
-        res["videos"] = self.video_list
+    def GET(self, path=None, **params):
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
 
-        cherrypy.response.headers['Content-Type'] = "application/json"
+        # Call for video list
+        if path is None:
+            cherrypy.response.headers['Content-Type'] = "application/json"
+            return json.dumps({"videos": self.video_list}).encode('utf8')
+        elif path[0] == 'video':
+            # Check for params
+            if params['id'] is not None and params['id'] in self.video_list:
+                cherrypy.response.headers['Content-Type'] = "video/mp4"
+                return self.load_video(params['id'])
 
-        return json.dumps(res).encode('utf8')
+    def load_video(self, video_id):
+        with open(self.video_path + video_id, 'rb') as video_stream:
+            return video_stream.read()
+
